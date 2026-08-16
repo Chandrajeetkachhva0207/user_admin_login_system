40ddbf55ff541e88dd5f2e24bcb5456002e3fcf2
chandrajeet kachhva <chandrajeetkachhva@gmail.com>
2026-08-16 10:45:18 +0530

    Update db.js SSL config and package.json

--- db.js
+++ db.js
@@ -1,8 +1,22 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+
+pool.on("connect", () => {
+  console.log("PostgreSQL database connected successfully.");
+});
+
+
+pool.on("error", (err) => {
+  console.error("PostgreSQL database error:", err);
+});
+
 
 module.exports = pool;
